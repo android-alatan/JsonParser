00df81bb912f991e207d627c3e3374c5d1bf056b
Steve Jung <steve.ugijung@krustuniverse.com>
2022-02-21 23:01:32 +0800

    (CHORE) update as jitpack docs

--- buildSrc/src/main/java/publish-base-lib.gradle.kts
+++ buildSrc/src/main/java/publish-base-lib.gradle.kts
@@ -5,20 +5,10 @@ plugins {
 afterEvaluate {
 
     val isAndroid = plugins.hasPlugin("com.android.library")
-    tasks.create("buildForDeploy") {
-        dependsOn(if (isAndroid) "assembleRelease" else "assemble")
-    }
 
     val artifactName: String = name
 
     if (artifactName.contains("sample")) return@afterEvaluate
-
-    val libVersion: String
-    if (project.hasProperty("libVersion")) {
-        libVersion = project.property("libVersion") as String
-    } else {
-        return@afterEvaluate
-    }
 
     publishing {
         publications {
@@ -30,20 +20,10 @@ afterEvaluate {
                 }
                 groupId = groupId()
                 artifactId = artifactName
-                this.version = libVersion
 
                 artifact(tasks.getByName("sourceJar"))
             }
 
-            repositories {
-                maven {
-                    url = uri("https://jitpack.io")
-                    credentials {
-                        // this should change to build local property manually once Gradle referring 1.5.0
-                        username = System.getenv("JITPACK_ACCESS_TOKEN")
-                    }
-                }
-            }
         }
     }
 
